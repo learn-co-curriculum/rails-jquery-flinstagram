5511dd66c2b0a98186bc315fc8d4ce7f6946d768
mrmitchboyer <mitch@mitchboyer.com>
2014-09-18 17:31:51 -0400

    removes solution

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,12 +3,7 @@ class PicturesController < ApplicationController
   def like
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-    message = @picture.likes_message(current_user)
-
-    respond_to do |format|
-      format.json { render json: {message: message} }
-      format.html { redirect_to root_path }
-    end
+    redirect_to root_path
   end
 
 end
