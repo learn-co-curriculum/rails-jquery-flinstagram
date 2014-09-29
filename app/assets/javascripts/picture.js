65520294ff4b691e7a02fd5657cc96fb800c209d
mrmitchboyer <mitch@mitchboyer.com>
2014-09-29 14:32:22 -0400

    adds js solution

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,26 @@
-// Add your JavaScript here
+$(document).ready(function(){
+
+  // listens for a button click
+  $('.like-btn').click(function(){
+    var $btn = $(this);
+    var pictureId = $btn.attr('picture_id');
+
+    // this function toggles the heart button and updates the text (called after ajax success)
+    function updateLikes($btn, results){
+      $btn.find('span').toggleClass('grey-heart red-heart');
+      $btn.parent().find('.likes-message').html(results.message);
+    }
+
+    // post request to the rails controller
+    $.ajax({
+      type: 'POST',
+      url:'/picture/' +pictureId+ '/like',
+      success: function(results){
+        updateLikes($btn, results);
+      }
+    });
+
+  });
+
+});
+
