ae88747d898b571a36c35ace3ba07a6ee35740a0
mrmitchboyer <mitch@mitchboyer.com>
2014-09-29 14:32:49 -0400

    updates controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,7 +3,12 @@ class PicturesController < ApplicationController
   def like
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-    redirect_to root_path
+    message = @picture.likes_message(current_user)
+
+    respond_to do |format|
+      format.json { render json: {message: message} }
+      format.html { redirect_to root_path }
+    end
   end
 
 end
