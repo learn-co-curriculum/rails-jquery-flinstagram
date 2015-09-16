8be9ab32f794b3337e6124168b4b5ac78c70e9d5
Drew Price <drewprice@users.noreply.github.com>
2015-09-16 15:12:59 -0400

    Add reload to Picture#update_likes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,13 +4,14 @@ class Picture < ActiveRecord::Base
 
   def update_likes(user)
     user_liked?(user) ? unlike(user) : like(user)
+    reload
   end
 
   def likes_message(user)
-    if ajax_fail || !user_liked?(user)
-      "<strong>#{self.likes.count} people</strong> like this image".html_safe
-    else
+    if user_liked?(user)
       "<strong>You</strong> and <strong>#{self.likes.count - 1} other people</strong> like this image".html_safe
+    else
+      "<strong>#{self.likes.count} people</strong> like this image".html_safe
     end
   end
 
@@ -24,23 +25,15 @@ class Picture < ActiveRecord::Base
 
   private
 
-      def user_liked?(user)
-        self.likes.any? do |like| 
-          like.user_id == user.id 
-        end
-      end
+  def user_liked?(user)
+    likes.any? { |like| like.user_id == user.id }
+  end
 
-      def like(user)
-        self.likes.create(user_id: user.id)
-      end
+  def like(user)
+    likes.create(user_id: user.id)
+  end
 
-      def unlike(user)
-        self.likes.find_by(user_id: user.id).destroy
-      end
-
-      def ajax_fail
-        # fixes error that occurs when user_liked? is called with ajax
-        self.likes.count != self.likes.length
-      end
-
+  def unlike(user)
+    likes.find_by(user_id: user.id).destroy
+  end
 end
