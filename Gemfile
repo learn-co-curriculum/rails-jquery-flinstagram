8d80b3ac3dcab426c82bcf65dd6526e19c0778a6
Steven Nunez <steven.nunez@gmail.com>
2015-09-29 11:29:47 -0400

    Force query to look in DB

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.2.0'
+ruby '2.2.3'
 
 gem 'rails', '4.2'
 gem 'sass-rails', '4.0.2'
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -7,7 +7,7 @@ class Picture < ActiveRecord::Base
   end
 
   def likes_message(user)
-    if ajax_fail || !user_liked?(user)
+    if !user_liked?(user)
       "<strong>#{self.likes.count} people</strong> like this image".html_safe
     else
       "<strong>You</strong> and <strong>#{self.likes.count - 1} other people</strong> like this image".html_safe
@@ -23,24 +23,15 @@ class Picture < ActiveRecord::Base
   end
 
   private
+  def user_liked?(user)
+    self.likes.where(user: user).any?
+  end
 
-      def user_liked?(user)
-        self.likes.any? do |like| 
-          like.user_id == user.id 
-        end
-      end
+  def like(user)
+    self.likes.create(user_id: user.id)
+  end
 
-      def like(user)
-        self.likes.create(user_id: user.id)
-      end
-
-      def unlike(user)
-        self.likes.find_by(user_id: user.id).destroy
-      end
-
-      def ajax_fail
-        # fixes error that occurs when user_liked? is called with ajax
-        self.likes.count != self.likes.length
-      end
-
+  def unlike(user)
+    self.likes.find_by(user_id: user.id).destroy
+  end
 end
